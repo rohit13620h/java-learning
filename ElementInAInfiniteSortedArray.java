3d494690fd9b146d9936223d6f88acbbadc99859
rohit13620h <rohit13620h@gmail.com>
2025-06-15 21:44:36 +0545

    was using linear search now maybe  using binary search

--- ElementInAInfiniteSortedArray.java
+++ ElementInAInfiniteSortedArray.java
@@ -17,8 +17,9 @@ public class ElementInAInfiniteSortedArray {
         System.out.println("enter the element you want to search for");
         int target = in.nextInt();
 
-        int position = elementposition(givenarray,target);
+//        int position = elementposition(givenarray,target); // it thought i used binary search but it was linear
 
+        int position = getrange(givenarray,target);
 
         System.out.println(Arrays.toString(givenarray));
         System.out.println(position);
@@ -47,7 +48,7 @@ public class ElementInAInfiniteSortedArray {
 
     static void arrangedata(int[] array){
         for(int i = 0 ; i < array.length-1 ; i++){
-            int temp=0;
+            int temp;
             for(int j = i+1 ; j < array.length ; j++){
                 if(array[i]>array[j]){
                     temp=array[i];
@@ -59,37 +60,93 @@ public class ElementInAInfiniteSortedArray {
         }
     }
 
-    static int elementposition(int[] array,int target){
-        int n = 0,m=0;
-
+    static int getrange(int[] array,int target){
+        int start = 0,end=1;
         while(true){
-//         if(array[m]=='\0'){
-//             m=m-1;
-//         }else{
-             if(array[m]<target){
-                 if(array[m+1]>=Integer.MIN_VALUE){
-                     n=m+1;
-                     m=m+1;
-                 }else{
-                     return -1;
-                 }
-             }else{
-                 int mid;
-                 while(true){
-                     mid=n+(m-n)/2;
-                     if(n>m){
-                         return -1;
-                     }
-                     if(array[mid]==target){
-                         return mid;
-                     }else if(array[mid]<target){
-                         n=mid+1;
-                     }else{
-                         m=mid-1;
-                     }
-                 }
-             }
-//         }
+            if (check(end,array)){
+                    end=end-1-(end-start)/2;
+                continue;
+            }else if(start==end){
+                if(check(end+1,array)){
+                    if(target==array[end]){
+                        return end;
+                    }
+                    return -1;
+                }
+            }
+            if(target<=array[end]){
+                return binarysearch(array,target,start,end);
+            }else{
+                start=end;
+                end=3*end/2+1;
+            }
         }
     }
+
+    static boolean check(int index,int[] array){
+
+        try{
+            int i =array[index];
+            return false;
+
+        }catch(ArrayIndexOutOfBoundsException e){
+            return true;
+        }
+    }
+
+    static int binarysearch(int[] array,int target,int start,int end){
+        int mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(array[mid]==target){
+                return mid;
+            }else if(array[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+//    static int elementposition(int[] array,int target){
+//
+//
+//        //this is linear search
+//        int n = 0,m=0;
+//
+//        while(true){
+////         if(array[m]=='\0'){
+////             m=m-1;
+////         }else{
+//             if(array[m]<target){
+//                 if(array[m+1]>=Integer.MIN_VALUE){
+//                     n=m+1;
+//                     m=m+1;
+//                 }else{
+//                     return -1;
+//                 }
+//             }else{
+//                 if(array[n]==target){
+//                     return n;
+//                 }
+//
+////                 int mid;
+////                 while(true){
+////                     mid=n+(m-n)/2;
+////                     if(n>m){
+////                         return -1;
+////                     }
+////                     if(array[mid]==target){
+////                         return mid;
+////                     }else if(array[mid]<target){
+////                         n=mid+1;
+////                     }else{
+////                         m=mid-1;
+////                     }
+////                 }
+//             }
+////         }
+//        }
+//    }
 }
