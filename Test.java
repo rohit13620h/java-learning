ca0339c3a428ace0b1d362ad2f9809abd4a5a7f0
rohit13620h <rohit13620h@gmail.com>
2025-06-24 10:11:37 +0545

    trying somethings for merge sort but didnt work

--- Test.java
+++ Test.java
@@ -1,61 +1,61 @@
 package idea.src;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        boolean[] boolvalue = {true, false, false};
-        rotateboolvalue(boolvalue);
-        System.out.println(Arrays.toString(boolvalue));
+        int[] array = { 20, 15,9,5};
+        int[] newarray=array.clone();
+        sort(array,newarray,0,array.length-1);
+        if (array.length==2){
+            array=newarray;
+        }
+        System.out.println(Arrays.toString(array));
     }
 
-    static void rotateboolvalue(boolean[] boolvalue) {
-        boolean temp;
-        int len = boolvalue.length;
-        for (int i = 0; i < len; i++) {
-            temp = boolvalue[i];
-            boolvalue[i] = boolvalue[len - 1 - i];
-            boolvalue[len - 1 - i] = temp;
-            if (i == len - 1 - i) {
+    static void sort(int[] array,int[] newarr, int start, int end){
+            if(end-start<=1){
+                if(newarr[start]>newarr[end]){
+                    swap(newarr,start,end);
+                }
+            }else{
+                int mid=start+(end-start)/2;
+                sort(newarr,array,start,mid);
+                sort(newarr,array,mid+1,end);
+                merge(array,newarr,start,mid,mid+1,end);
+            }
+
+    }
+
+    static void merge(int[] array,int[] newarray,int start,int mid,int aftermid,int end){
+        for(int i = start ; i<=end ; i++){
+            if(newarray[start]>newarray[aftermid]){
+                array[i]=newarray[aftermid];
+                aftermid++;
+            }else{
+                array[i]=newarray[start];
+                start++;
+            }
+            if(start>mid){
+                for(int j=i+1;j<=end;j++){
+                    array[j]=newarray[aftermid];
+                    aftermid++;
+                }
+                break;
+            }else if(aftermid>end){
+                for(int j =i+1;j<=end;j++){
+                    array[j]=newarray[start];
+                    start++;
+                }
                 break;
             }
+
         }
     }
+
+    static void swap(int[] array,int index,int last){
+        int temp=array[index];
+        array[index]=array[last];
+        array[last]=temp;
+    }
 }
-//        ArrayList<Integer> list = new ArrayList<>(10);
-//        list.add(101);
-//        list.add(125);
-//        list.add(238);
-//        list.add(56);
-//        list.add(568);
-//        list.add(658);
-//        System.out.println(list.size());
-//        int[] arr = new int[10];
-//        arr[0] = 1;
-//        arr[1] = 41;
-//        arr[2] = 1;
-//        arr[3] = 51;
-//        arr[4] = 1;
-//        arr[5] = 1;
-//        arr[6] = 1;
-//        arr[7] = 51;
-//        arr[8] = 1;
-//        arr[9] = 1;
-//
-//        int[] arr2 = new int[20];
-//        arr2[17]=20;
-//        System.arraycopy(arr,0,arr2,0,10);
-////        arr=Arrays.copyOf(arr,20);
-//
-//        System.out.println(Arrays.toString(arr2));
-////        arr[17]=9;
-//        System.out.println(Arrays.toString(arr));
-//        System.out.println(arr[17]);
-
-
-//        arr2=arr.clone();
-//        System.out.println(Arrays.toString(arr2));
-//        System.out.println(arr2[17]);
-
-
