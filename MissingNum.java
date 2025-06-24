3293c7bae69f5918c4f9a8ad0dd8feeda9290f60
rohit13620h <rohit13620h@gmail.com>
2025-06-24 18:50:53 +0545

    now implemented cycle sort

--- MissingNum.java
+++ MissingNum.java
@@ -1,12 +1,9 @@
 package idea.src;
 //suppose a array for 0 to N but with a num missing i.e. size of array = n,now find the missing num
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class MissingNum {
     public static void main(String[] args) {
-        int[] array = {7,6,0,3,2,5,4,8,1};
+        int[] array = {7,6,0,3,2,5,9,4,10,12,15,14,13,8,1};
         int num = missingnum(array);
         System.out.println(num);
     }
@@ -23,16 +20,18 @@ public class MissingNum {
         return ans;
     }
     static void sort(int[] array){
-        for (int i = 0 ;i<array.length-1;i++){
-            for(int j = i;j>=0;j--){
-                if(array[j+1]<array[j]){
-                    swap(array,j+1,j);
-                }else{
-                    break;
-                }
-            }
+        int i=0;
+        while(i<array.length){
+        if(array[i]!=i){
+         if(array[i]== array.length){
+             i++;
+             continue;
+         }
+            swap(array,array[i],i);
+        }else{
+            i++;
         }
-
+        }
     }
     static void swap(int[] array,int first,int second){
         int temp = array[first];
