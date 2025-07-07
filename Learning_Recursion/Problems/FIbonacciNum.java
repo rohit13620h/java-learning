a4fbb64fa921bafd3a974ff308503dcb9fd069b2
rohit13620h <rohit13620h@gmail.com>
2025-07-07 09:34:53 +0545

    tried using dinamic programming in fibonaci recursion

--- Learning_Recursion/Problems/FIbonacciNum.java
+++ Learning_Recursion/Problems/FIbonacciNum.java
@@ -8,18 +8,32 @@ public class FIbonacciNum {
         System.out.println("enter the index which fibo num u want");
         Scanner input =new Scanner(System.in);
         int index= input.nextInt();
-        int and = fibonum(index);
+        int[] array=new int[index+1];
+
+        int and = fibonum(index,array);
 
         System.out.println(and);
     }
 
-    static int fibonum(int index){
+    static int fibonum(int index,int[] array){
+
         if(index==0){
+            array[0]=0;
             return 0;
         }else if(index==1){
+            array[1]=1;
             return 1;
         }else{
-            return fibonum(index-1)+fibonum(index-2);
+
+            if(array[index-1]==0){
+                array[index-1]=fibonum(index-1,array);
+            }
+            if(array[index-2]==0){
+                array[index-2]=fibonum(index-2,array);
+            }
+
+            array[index]=array[index-1]+array[index-2];
+            return array[index];
         }
     }
 
